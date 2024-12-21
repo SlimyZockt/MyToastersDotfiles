f0c5e399f55c060eaa3876e780afe8329fe6bef0
slimyzockt77 <slimyzockt77@gmail.com>
2024-12-21 14:50:10 +0100

    56

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -87,7 +87,9 @@ in
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
+  services.devmon.enable = true;
+  services.gvfs.enable = true;
+  services.udisks2.enable = true;
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
