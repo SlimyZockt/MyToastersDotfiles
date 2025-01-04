8dc377c6a962117ffd5af99c1e683a32ef525154
slimyzockt77 <slimyzockt77@gmail.com>
2025-01-04 15:17:27 +0100

    69

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+    set -g fish_key_bindings fish_vi_key_bindings
     set fish_greeting ""
         # Commands to run in interactive sessions can go here
     starship init fish | source
