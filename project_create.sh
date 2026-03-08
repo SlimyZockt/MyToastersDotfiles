e6316ca49ba188373992b23d5eb951302c003f45
slimyzockt77 <slimyzockt77@gmail.com>
2026-03-08 21:58:51 +0100

    162

--- project_create.sh
+++ project_create.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/env bash
 SELECTED=$(fuzzel -d --prompt-only="New project name: ")
+PROJECT_PATH=$HOME/Documents/$SELECTED/
 
-if [ ! -d "$SELECTED"] && [-n "$SELECTED"]; then
-    mkdir $SELECTED
-    cd $SELECTED
+if [ -n "$SELECTED" ] && [ ! -d "$PROJECT_PATH" ] ;then
+    mkdir -p $PROJECT_PATH
+    cd $PROJECT_PATH
     git init
-else
-    echo "Error: could not create the project"
+    echo "Created the project $SELECTED in $PROJECT_PATH"
+else 
+    echo "Error: could not create the project $SELECTED in $PROJECT_PATH. Project already exists."
 fi
